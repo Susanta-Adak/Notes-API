0649ccffdd2b8e024967dcb5a268d3ae716e7058
Susanta Adak <susantaadak513@gmail.com>
2022-11-20 10:15:45 +0530

    second commit

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,7 @@ dotenv.config();
 import cors from 'cors';
 
 const app = express();
-app.use(cors({
-    origin: "*",
-    methods: "GET, POST"
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
